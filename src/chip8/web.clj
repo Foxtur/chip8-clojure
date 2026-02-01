79017bf04f768dabb1fd4c03850c135b63a449c9
Dominik Schenner <admin@foxtur.com>
2026-02-01 20:30:58 +0100

    [feat] optimized rendering for lower network usage

--- src/chip8/web.clj
+++ src/chip8/web.clj
@@ -90,7 +90,7 @@
                     "     data-on:keyup__window=\"@get('/input?sid=" sid "&key=' + evt.key + '&type=up')\">"
                     "  <div data-init=\"@get('/stream?sid=" sid "')\">"
                     "    <div id='display-container'>"
-                    "      Connecting..."
+                    "      <svg id='display' width='640' height='320' viewBox='0 0 64 32' style='background:black;'></svg>"
                     "    </div>"
                     "  </div>"
                     "</div>"))
@@ -122,32 +122,7 @@
         [:div {:data-init "@get('/stream')"}
          [:div#display "Connecting..."]]]])))
 
-(defn render-display [cpu]
-  (let [display (:display cpu)
-        width 64
-        height 32
-        scale 10
-        sound-active? (and (> (:sound cpu) 0) (< (:sound cpu) 5))]
-    (str
-     (h/html
-      [:div#display-container
-          ;; the screen
-       [:svg#display {:width (* width scale)
-                      :height (* height scale)
-                      :viewbox (str "0 0 " width " " height)
-                      :style "background: black; shape-rendering: crispEdges;"}
-        (for [idx (range (count display))
-              :let [pixel (nth display idx)]
-              :when (= pixel 1)
-              :let [x (mod idx width)
-                    y (quot idx width)]]
-          [:rect {:x x :y y :width 1 :height 1 :fill "#00FF41"}])]
-          ;; sound trigger if timer is > 0
-       (when sound-active?
-         [:audio {:src "/soundeffect.mp3"
-                  :autoplay true}])]))))
-
-(defn render-display-divs [cpu]
+(defn render-display-divs-naive [cpu]
   (let [display (:display cpu)]
     (str
      (h/html
@@ -155,13 +130,63 @@
        (for [pixel display]
          [:div {:class (str "pixel " (if (= pixel 1) "on" "off"))}])]))))
 
+(defn render-display [display sound-active? paused?]
+  (let [width 64 height 32 scale 10]
+    (str
+     (h/html
+      [:div#display-container
+       [:svg#display {:width (* width scale) :height (* height scale) :viewBox "0 0 64 32"
+                      :style "background: black; shape-rendering: crispEdges;"}
+        ;; Draw pixels
+        (for [idx (range (count display))
+              :when (= 1 (nth display idx))]
+          [:rect {:x (mod idx 64) :y (quot idx 64) :width 1 :height 1 :fill "#00FF41"}])
+        
+        ;; Draw Pause Text
+        (when paused?
+          [:text {:x 32 :y 16 :fill "red" :font-size "5" 
+                  :text-anchor "middle" :font-family "monospace"} "PAUSED"])]
+       
+       (when (and sound-active? (not paused?))
+         [:audio {:id (str "beep-" (System/currentTimeMillis))
+                  :src "/soundeffect.mp3" :autoplay true}])]))))
+
+(defn render-display-optimized [display sound-active? paused?]
+  (let [width 64 height 32 scale 10]
+    (str
+     (h/html
+      ;; We target the display-container, but we use "morph" merge
+      ;; to ensure Datastar only touches what changed.
+      [:div#display-container {:data-merge "morph"}
+       [:svg#display {:width (* width scale) :height (* height scale) :viewBox "0 0 64 32"
+                      :style "background: black; shape-rendering: crispEdges;"}
+        
+        ;; OPTIMIZATION: Only send the pixels that are ON.
+        ;; Morphing will remove the ones that disappear.
+        (for [idx (range (count display))
+              :let [pixel (nth display idx)]
+              :when (= pixel 1)]
+          [:rect {:id (str "p-" idx) ;; ID is key for granular morphing performance
+                  :x (mod idx width) :y (quot idx width) 
+                  :width 1 :height 1 :fill "#00FF41"}])
+        
+        ;; Draw Pause Text
+        (when paused?
+          [:text {:id "pause-label" :x 32 :y 16 :fill "red" :font-size "5" 
+                  :text-anchor "middle" :font-family "monospace"} "PAUSED"])]
+       
+       ;; Sound Trigger: ID must be unique to force fresh play
+       (when (and sound-active? (not paused?))
+         [:audio {:id (str "beep-" (System/currentTimeMillis))
+                  :src "/soundeffect.mp3" :autoplay true}])]))))
+
 (defn send-fragment! [channel html-str]
   (server/send! channel
                 (str "event: datastar-patch-elements\n"
                      "data: elements " html-str "\n\n")
                 false))
 
-(defn stream-handler [{:keys [params] :as req}]
+(defn stream-handler-naive [{:keys [params] :as req}]
   (let [sid (get params "sid")]
     (println "Stream connected for SID:" sid)
     (server/with-channel req channel
@@ -171,26 +196,71 @@
         (future
           (loop [last-display nil]
             (if (server/open? channel)
-              (let [next-cpu (-> (nth (iterate cpu/step @user-cpu) 10) cpu/decrement-timers)]
+              (let [next-cpu (-> (nth (iterate cpu/step @user-cpu) 10) cpu/decrement-timers)
+                    sound-active? (and (> (:sound next-cpu) 0) (< (:sound next-cpu) 5))]
                 (reset! user-cpu next-cpu)
                 (when (not= last-display (:display next-cpu))
-                  (send-fragment! channel (render-display next-cpu)))
+                  (send-fragment! channel (render-display next-cpu sound-active?)))
                 (Thread/sleep 16)
                 (recur (:display next-cpu)))
               (swap! active-games dissoc sid))))))))
 
-(defn input-handler [{:keys [params] :as req}]
+(defn stream-handler [{:keys [params] :as req}]
+  (let [sid (get params "sid")]
+    (server/with-channel req channel
+      (server/send! channel {:headers {"Content-Type" "text/event-stream"}} false)
+      (let [user-cpu (atom (-> (cpu/init-cpu) (cpu/load-rom "resources/roms/IBM Logo.ch8")))]
+        (swap! active-games assoc sid user-cpu)
+        (future
+          (loop [last-display nil
+                 last-sound 0
+                 last-paused nil] ;; Track pause state to force a render update
+            (if (server/open? channel)
+              (let [cpu-state @user-cpu
+                    paused? (:paused? cpu-state)
+                    
+                    ;; 1. Logic: Only step if NOT paused
+                    next-cpu (if paused?
+                               cpu-state
+                               (-> (nth (iterate cpu/step cpu-state) 10) 
+                                   cpu/decrement-timers))
+                    
+                    ;; 2. Update the atom with new state
+                    _ (when-not paused? (reset! user-cpu next-cpu))
+                    
+                    curr-display (:display next-cpu)
+                    curr-sound (:sound next-cpu)]
+
+                ;; 3. Render: Send if display, sound, or PAUSE state changed
+                (when (or (not= last-display curr-display)
+                          (and (= last-sound 0) (> curr-sound 0))
+                          (not= last-paused paused?))
+                  (send-fragment! channel (render-display-optimized curr-display (> curr-sound 0) paused?)))
+
+                (Thread/sleep 16)
+                (recur curr-display curr-sound paused?))
+              (swap! active-games dissoc sid))))))))
+
+(defn input-handler [{:keys [params]}]
   (let [sid (get params "sid")
         user-atom (get @active-games sid)]
     (when user-atom
-      (let [key-str (get params "key")
-            key-char (when (= 1 (count key-str)) (first key-str))
-            hex (get cpu/key-map key-char)]
-        (when hex
-          (if (= (get params "type") "up")
-            (swap! user-atom update :keypad disj hex)
-            (swap! user-atom update :keypad conj hex)))))
-    {:status 204}))
+      (let [key-val (get params "key")
+            type (get params "type")]
+        (cond
+          ;; Toggle pause on 'p' keydown
+          (and (= key-val "p") (not= type "up"))
+          (swap! user-atom update :paused? not)
+
+          ;; Handle regular keypad keys
+          :else
+          (let [key-char (when (= 1 (count key-val)) (first key-val))
+                hex (get cpu/key-map key-char)]
+            (when hex
+              (if (= type "up")
+                (swap! user-atom update :keypad disj hex)
+                (swap! user-atom update :keypad conj hex)))))))
+    {:status 204 :body ""}))
 
 (defn load-handler [{:keys [params]}]
   (let [sid (get params "sid")
@@ -204,7 +274,6 @@
       (reset! user-atom (-> (cpu/init-cpu)
                             (cpu/load-rom (str "resources/roms/" rom-name)))))
     {:status 204 :body ""}))
-
 
 (defn app-handler [{:keys [uri] :as req}]
   (case uri
